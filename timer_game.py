22c7bbc4bd751a6101c95ba57b42ad075d36a29a
Máté Szendrő <mate@rollet.hu>
2018-11-20 10:49:07 +0100

    Run the game repeatedly

--- timer_game.py
+++ timer_game.py
@@ -1,21 +1,26 @@
 import time
 
 
-expected_time = input("How fast do you think you are? (in seconds) ")
-if expected_time != "":
-    expected_time = int(expected_time)
+while True:
+    expected_time = input("How fast do you think you are? (in seconds) ")
+    if expected_time != "":
+        expected_time = int(expected_time)
 
-    input("Press enter to start!")
-    start_time = time.time()
+        input("Press enter to start!")
+        start_time = time.time()
 
-    input("Measuring... press enter to stop!")
-    end_time = time.time()
+        input("Measuring... press enter to stop!")
+        end_time = time.time()
 
-    duration = end_time - start_time
-    if expected_time * 0.8 <= duration <= expected_time * 1.2:
-        print("You were right :)")
+        duration = round(end_time - start_time, 1)
+        if expected_time * 0.8 <= duration <= expected_time * 1.2:
+            print("You were right :)")
+        else:
+            print("You were wrong :(")
+            print("It took you " + str(duration) + " seconds")
+
+        answer = input("Do you want to play a new game? (y/n) ")
+        if answer != "y":
+            break
     else:
-        print("You were wrong :(")
-        print("It took you " + str(duration) + " seconds")
-else:
-    print("Enter a number please!")
+        print("Enter a number please!")
